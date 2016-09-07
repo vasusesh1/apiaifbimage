748bd3ca4d83860255fd5b35b597ccac7ac037c3
vasusesh1 <vasupradhaseshadri@gmail.com>
2016-09-07 07:25:38 -0700

    new edits

--- src/app.js
+++ src/app.js
@@ -270,7 +270,11 @@ app.post('/webhook/', (req, res) => {
                         if (event.message && !event.message.is_echo ||
                             event.postback && event.postback.payload) {
                             processEvent(event);
-                            //sendGenericMessage(sender);
+                            if (text === 'Generic') {
+                            sendGenericMessage(sender)
+                            continue
+                            }
+                            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
                         }
                     });
                 }
